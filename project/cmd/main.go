7de4453927986d291ffca743053534a335d4eda2
Ggh13 <roma_bzuhor@mail.ru>
2024-11-15 19:50:04 +0300

    add reg_auth for corporation

--- project/cmd/main.go
+++ project/cmd/main.go
@@ -28,10 +28,23 @@ import( "github.com/gorilla/mux"
 		"github.com/gorilla/sessions"
 		)
 
+type Auth_data struct{
+	Is_that_authorized bool
+
+	Is_that_employee bool
+	Employee User
+	Corporation Corporation_data
+}
+
 type User struct{
    Id, Firstname, Surname,Secondname, Email, Password, Date_birth_day, Time_birth, City_of_birth, Work_experience string
    Is_that_authorized_user bool
    Is_it_admin bool
+}
+
+type Corporation_data struct{
+	Id, Email, Password, Name string
+	Employees []User
 }
 var store = sessions.NewCookieStore([]byte("super-secret-key"))
 
@@ -45,7 +58,7 @@ var adress_data_base_test = adress_sql + "naimix"
 
 
 
-func populateUserFromSession(r *http.Request, sessionName string) (User, error) {
+func populateUserFromSession(r *http.Request, sessionName string) (Auth_data, error) {
     session, err := store.Get(r, sessionName)
     if err != nil{
       panic(err)
@@ -69,33 +82,34 @@ func populateUserFromSession(r *http.Request, sessionName string) (User, error) 
 
 
     }
-
-    return user, nil
+		var auth_data Auth_data
+		auth_data.Employee = user
+    return auth_data, nil
 }
 
 
 
-func saveUserToSession(r *http.Request, w http.ResponseWriter, sessionName string, user User) error{
+func saveUserToSession(r *http.Request, w http.ResponseWriter, sessionName string, user Auth_data) error{
     session, err := store.Get(r, sessionName)
     if err != nil {
         return err
     }
 
 
-    session.Values["Curret_user_id"] = user.Id
-    session.Values["Firstname"] = user.Firstname
-    session.Values["Secondname"] = user.Secondname
-    session.Values["Surname"] = user.Surname
+    session.Values["Curret_user_id"] = user.Employee.Id
+    session.Values["Firstname"] = user.Employee.Firstname
+    session.Values["Secondname"] = user.Employee.Secondname
+    session.Values["Surname"] = user.Employee.Surname
 
-		session.Values["Date_birth_day"] = user.Date_birth_day
-		session.Values["Time_birth"] = user.Time_birth
-		session.Values["City_of_birth"] = user.City_of_birth
-		session.Values["Work_experience"] = user.Work_experience
+		session.Values["Date_birth_day"] = user.Employee.Date_birth_day
+		session.Values["Time_birth"] = user.Employee.Time_birth
+		session.Values["City_of_birth"] = user.Employee.City_of_birth
+		session.Values["Work_experience"] = user.Employee.Work_experience
 
 
-    session.Values["Email"] = user.Email
-		session.Values["Password"] = user.Password
-    session.Values["Is_authorized"] = user.Is_that_authorized_user
+    session.Values["Email"] = user.Employee.Email
+		session.Values["Password"] = user.Employee.Password
+    session.Values["Is_authorized"] = user.Employee.Is_that_authorized_user
 
 
 
@@ -126,6 +140,26 @@ func  get_user_employee_by_id(id string) (User){
 }
 
 
+
+func  get_user_corporation_by_id(id string) (Corporation_data){
+
+	db, err := sql.Open("mysql", adress_data_base_test)
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
+
+
+	var zapros = fmt.Sprintf("SELECT id, name FROM `corporations` WHERE id = %s", id)
+	res,_ := db.Query(zapros)
+	fmt.Println(zapros)
+	var user Corporation_data
+
+	for res.Next(){
+		err = res.Scan(&user.Id,&user.Name)
+	}
+	return user
+}
 
 
 func returnToLastPage(r *http.Request, w http.ResponseWriter){
@@ -184,16 +218,12 @@ func employee_profile(w http.ResponseWriter, r *http.Request){
 	fmt.Println(user)
 }
 
-func authorization_page(w http.ResponseWriter, r *http.Request){
-	t, err := template.ParseFiles("../frontend/templates/authorization_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
+func employee_authorization_page(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("../frontend/templates/employee_authorization_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
 	if err != nil{
 		fmt.Fprintf(w, err.Error())
 
 	}
-
-
-
-
 		db, err := sql.Open("mysql", adress_data_base_test)
 		if err != nil{
 			panic(err)
@@ -207,22 +237,23 @@ func authorization_page(w http.ResponseWriter, r *http.Request){
 			var zapros = fmt.Sprintf("SELECT id, firstname, secondname, surname FROM `users` WHERE email = %s AND password = %s", email, password)
 			res,_ := db.Query(zapros)
 			fmt.Println(zapros)
-			var user User
-
+			var user Auth_data
+			user.Is_that_employee = true
 			for res.Next(){
-				err = res.Scan(&user.Id,&user.Firstname,&user.Secondname,&user.Surname)
+				err = res.Scan(&user.Employee.Id,&user.Employee.Firstname,&user.Employee.Secondname,&user.Employee.Surname)
 			}
-			user.Is_that_authorized_user = true
+
+			user.Employee.Is_that_authorized_user = true
 			saveUserToSession(r, w, sessionName, user)
-			http.Redirect(w, r, "/employee_profile/" + user.Id, http.StatusSeeOther)
+			http.Redirect(w, r, "/employee_profile/" + user.Employee.Id, http.StatusSeeOther)
 		}
 
 
-	t.ExecuteTemplate(w, "authorization_page", nil)
+	t.ExecuteTemplate(w, "employee_authorization_page", nil)
 }
 
-func registration_page(w http.ResponseWriter, r *http.Request){
-      t, err := template.ParseFiles("../frontend/templates/registration_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
+func employee_registration_page(w http.ResponseWriter, r *http.Request){
+      t, err := template.ParseFiles("../frontend/templates/employee_registration_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
       if err != nil{
         fmt.Fprintf(w, err.Error())
 
@@ -271,9 +302,103 @@ func registration_page(w http.ResponseWriter, r *http.Request){
 
       //authorized_user, err  := populateUserFromSession(r, sessionName)
       //data.Authorized_user_data  = authorized_user
-      t.ExecuteTemplate(w, "registration_page", nil)
+      t.ExecuteTemplate(w, "employee_registration_page", nil)
 }
 
+
+
+
+func corporation_registration_page(w http.ResponseWriter, r *http.Request){
+      t, err := template.ParseFiles("../frontend/templates/corporation_registration_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
+      if err != nil{
+        fmt.Fprintf(w, err.Error())
+
+      }
+
+
+				db, err := sql.Open("mysql", adress_data_base_test)
+			  if err != nil{
+			    panic(err)
+			  }
+			  defer db.Close()
+
+
+			if r.Method == http.MethodPost {
+				name := r.FormValue("name")
+				email := r.FormValue("email")
+				//weight := r.FormValue("weight")
+				password := r.FormValue("password")
+
+					result, err := db.Exec("insert into naimix.corporations ( `name`,	`email`, `password`) values (?, ?,?)",name, email, password)
+
+					fmt.Println(result)
+					if(err != nil){
+						fmt.Println(err)
+					}
+
+
+
+			}
+
+      //var data Data_for_personal_page
+
+      //authorized_user, err  := populateUserFromSession(r, sessionName)
+      //data.Authorized_user_data  = authorized_user
+      t.ExecuteTemplate(w, "corporation_registration_page", nil)
+}
+
+func corporation_profile(w http.ResponseWriter, r *http.Request){
+
+	vars := mux.Vars(r)
+  w.WriteHeader(http.StatusOK)
+//  fmt.Fprintf(w, "Category: %v\n", vars["id_user"])
+  var current_user_id = vars["id_corporation"]
+
+	fmt.Println("prof")
+	fmt.Println(current_user_id)
+	user := get_user_corporation_by_id(current_user_id)
+
+	//user, _  := populateUserFromSession(r, sessionName)
+	fmt.Println(user)
+}
+
+func corporation_authorization_page(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("../frontend/templates/corporation_authorization_page.html", "../frontend/templates/header.html", "../frontend/templates/footer.html")
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+
+	}
+
+
+
+
+		db, err := sql.Open("mysql", adress_data_base_test)
+		if err != nil{
+			panic(err)
+		}
+		defer db.Close()
+
+
+		if r.Method == http.MethodPost {
+			email := r.FormValue("email")
+			password := r.FormValue("password")
+			var zapros = fmt.Sprintf("SELECT id, name FROM `corporations` WHERE email = %s AND password = %s", email, password)
+			res,_ := db.Query(zapros)
+			fmt.Println(zapros)
+			var user Auth_data
+			user.Is_that_employee = true
+			for res.Next(){
+				err = res.Scan(&user.Corporation.Id,&user.Corporation.Name)
+			}
+
+
+			saveUserToSession(r, w, sessionName, user)
+			http.Redirect(w, r, "/corporation_profile/" + user.Corporation.Id, http.StatusSeeOther)
+		}
+
+
+	t.ExecuteTemplate(w, "corporation_authorization_page", nil)
+}
 
 
 func main() {
@@ -293,9 +418,15 @@ func main() {
 
 
 	r.HandleFunc("/", home_page)
-	r.HandleFunc("/registration_page", registration_page)
-	r.HandleFunc("/authorization_page", authorization_page)
+	r.HandleFunc("/employee_registration_page", employee_registration_page)
+	r.HandleFunc("/employee_authorization_page", employee_authorization_page)
 	r.HandleFunc("/employee_profile/{id_employee}", employee_profile)
+
+	r.HandleFunc("/corporation_registration_page", corporation_registration_page)
+	r.HandleFunc("/corporation_authorization_page", corporation_authorization_page)
+	r.HandleFunc("/corporation_profile/{id_corporation}", corporation_profile)
+
+
 
   http.Handle ("/", r)
   http.ListenAndServe(":8080", nil)
